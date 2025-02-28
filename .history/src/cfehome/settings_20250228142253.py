fb1c6bc90a3eb1b39f17102d4567870e725bf69d
Arturgouveia1970 <artgouveiag@gmail.com>
2025-02-28 16:27:30 +0000

    more shit

--- .history/src/cfehome/settings_20250228142253.py
+++ .history/src/cfehome/settings_20250228142253.py
@@ -17,13 +17,13 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Email config
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = config("EMAIL_HOST", cast=str, default="smtp.gmail.com")
-EMAIL_PORT = config("EMAIL_PORT", cast=str, default="587") # Recommended
-EMAIL_HOST_USER = config("EMAIL_HOST_USER", cast=str, default=None)
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", cast=str, default=None)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)  # Use EMAIL_PORT 587 for TLS
-EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool, default=False)  # Use MAIL_PORT 465 for SSL
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT="587"
+EMAIL_USE_TLS=True
+EMAIL_USE_TLS=F # 465
+
+EMAIL_HOST_USER="artgouveiag@gmail.com"
+EMAIL_HOST_PASSWORD="Gmail App Password"
 
 
 
